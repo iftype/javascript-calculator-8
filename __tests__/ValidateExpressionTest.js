8ee4bc013114347d08cd1912ea02023e731d967e
iftype <iftype@naver.com>
2025-10-19 06:35:30 +0900

    test(ValidateExpression): 유닛 테스트 추가
    validIsInDelimList unit test
    
    표현식의 끝 부분이 숫자로 끝나는지 검증 메소드 유닛테스트추가

--- __tests__/ValidateExpressionTest.js
+++ __tests__/ValidateExpressionTest.js
@@ -2,6 +2,7 @@ import ValidateExpression from '../src/ValidateExpression.js';
 
 const valid = new ValidateExpression();
 
+//표현식검증 숫자로 시작하는지 테스트
 const vaildIsStartNumber = valid.vaildIsStartNumber.name;
 describe(vaildIsStartNumber, () => {
   test('에러발생 테스트', () => {
@@ -14,10 +15,24 @@ describe(vaildIsStartNumber, () => {
   });
 });
 
+//표현식검증 숫자로 끝나는지 테스트
 const vaildIsEndNumber = valid.vaildIsEndNumber.name;
 describe(vaildIsEndNumber, () => {
   test('에러발생 테스트', () => {
     const validator = new ValidateExpression('1,2,3,');
     expect(() => validator.vaildIsEndNumber()).toThrow(Error);
+  });
+});
+
+//표현식 검증, 특수문자 있는지 테스트
+const validIsInDelimList = valid.validIsInDelimList.name;
+describe(validIsInDelimList, () => {
+  test('에러발생 테스트', () => {
+    const validator = new ValidateExpression('1,2;3');
+    expect(() => validator.validIsInDelimList()).toThrow(Error);
+  });
+  test('패스 테스트', () => {
+    const validator = new ValidateExpression('1,2,3');
+    expect(() => validator.validIsInDelimList()).not.toThrow();
   });
 });
