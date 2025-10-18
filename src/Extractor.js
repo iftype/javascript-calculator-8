8fd3217a73e75482da73e2029c5be3b708dcdfa0
iftype <iftype@naver.com>
2025-10-19 02:33:35 +0900

    feat(Extractor): add extractCustomPart method
    커스텀 문자 파트를 잘라내는 메서드 추가
    
    문자열의 시작부분과 처음 숫자와 만나는곳까지를 커스텀 문자 파트라고 봤다.

--- src/Extractor.js
+++ src/Extractor.js
@@ -1,7 +1,7 @@
 import Pattern from './Pattern.js';
 import { HEAD_PATTERN, TAIL_PATTERN } from './constants/patternConstants.js';
 class Extractor {
-  //해당 element의 타입을 Number타입으로 바꾸고 안전한 정수인지 확인, 공백이라면 false를 출력한다
+  //해당 element의 타입을 Number타입으로 바꾸고 안전한 정수인지 확인, 공백이라면 false를 리턴한다
   static isNumber(element) {
     if (element.trim() === '') return false;
     return Number.isSafeInteger(Number(element));
@@ -9,6 +9,12 @@ class Extractor {
   //처음 숫자를 만나는 인덱스를 리턴
   static indexOfNumber(string) {
     return string.split('').findIndex((e) => this.isNumber(e));
+  }
+  //숫자를 만나는 곳을 찾아 첫 부분부터 잘라낸다, 만약 숫자를 찾아내지 못한다면 원래 문자열을 리턴한다
+  static extractCustomPart(string) {
+    const findNum = this.indexOfNumber(string);
+    if (findNum === -1) return string;
+    return string.slice(0, findNum);
   }
 }
 
