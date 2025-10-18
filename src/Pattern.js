53329958f93dbac2b106ed9f83156b6b85f4fa28
iftype <iftype@naver.com>
2025-10-19 01:50:27 +0900

    feat(Pattern): add hasTailPattern method
    문자열을 받고 TAIL_PATTERN의 포함여부를 반환한다

--- src/Pattern.js
+++ src/Pattern.js
@@ -1,8 +1,12 @@
 import { HEAD_PATTERN, TAIL_PATTERN } from './constants/patternConstants.js';
 class Pattern {
-  //해당 문자열이 HEAD_PATTERN으로 시작하는지 확인
+  //문자열이 HEAD_PATTERN으로 시작하는지 확인
   static isStartHeadPattern(string) {
     return string.startsWith(HEAD_PATTERN);
+  }
+  //문자열에 TAIL_PATTERN을 포함하는지 확인
+  static hasTailPattern(string) {
+    return string.includes(TAIL_PATTERN);
   }
 }
 export default Pattern;
