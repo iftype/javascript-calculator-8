485eee558f61ea3a90fadadbc6d1cece4e0380c7
iftype <iftype@naver.com>
2025-10-19 01:57:32 +0900

    test(Pattern): add unit tests for hasPatterns   method
    hasPatterns의 성공테스트와 실패테스트를 추가했다

--- __tests__/PatternTest.js
+++ __tests__/PatternTest.js
@@ -25,3 +25,16 @@ describe('Pattern.js 테스트', () => {
     expect(result).toBe(false);
   });
 });
+
+describe('Pattern.js 테스트', () => {
+  test(`성공 테스트`, () => {
+    const string = '//*\\n';
+    const result = Pattern.hasPatterns(string);
+    expect(result).toBe(true);
+  });
+  test(`실패 테스트`, () => {
+    const string = '/*\\n';
+    const result = Pattern.hasPatterns(string);
+    expect(result).toBe(false);
+  });
+});
