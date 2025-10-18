12961f5597e677942e5792f7363818a5f8308490
iftype <iftype@naver.com>
2025-10-19 06:03:14 +0900

    feat(ValidateExpression): add vaildIsStartNumber method
    표현식의 첫부분이 숫자인지 판별

--- src/ValidateExpression.js
+++ src/ValidateExpression.js
@@ -1,5 +1,16 @@
 //표현식 부분 검사하는 클래스
 class ValidateExpression {
-  constructor() {}
+  //자기 자신을 리턴받기위해 멤버변수를 가짐
+  constructor(string) {
+    this.string = string || '';
+  }
+
+  //표현식의 첫 부분이 숫자인지 판별 리팩토링때 utils로 뺄 생각 중
+  vaildIsStartNumber() {
+    if (!Number.isInteger(Number(this.string[0])))
+      throw new Error('식의 첫 부분은 숫자여야 합니다');
+    return this;
+  }
 }
+
 export default ValidateExpression;
