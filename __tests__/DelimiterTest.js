923d8115a9ae3bf9b5e20115813136499083952e
iftype <iftype@naver.com>
2025-10-19 05:36:59 +0900

    test(Delimiter): add unit tests for splitDelimExpression method

--- __tests__/DelimiterTest.js
+++ __tests__/DelimiterTest.js
@@ -17,3 +17,13 @@ describe(hasDelimiterString, () => {
     expect(result).toBe(false);
   });
 });
+
+const splitDelimExpression = delimiter.splitDelimExpression;
+describe(splitDelimExpression, () => {
+  test('성공 테스트', () => {
+    const string = ',1,;,3,';
+    const delim = new Delimiter();
+    const result = delim.splitDelimExpression(string);
+    expect(result).toStrictEqual([' ', '1', ' ', ';', ' ', '3', ' ']);
+  });
+});
