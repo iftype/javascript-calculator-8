8ef0e3e6c471c2a4636ffc2afb0e943a39d4667c
iftype <iftype@naver.com>
2025-10-19 03:15:33 +0900

    feat(Calculator): Calculator 클래스 생성
    입력값을 받아 생성되는 클래스, 클래스들의 조립을 위해 만들어짐
    
    전체 진행과정을 담으려고 만듬

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import Calculator from './Calculator.js';
 
 class App {
   async run() {
@@ -6,7 +7,10 @@ class App {
       const userInput = await Console.readLineAsync(
         '덧셈할 문자열을 입력해 주세요.\n',
       );
-      Console.print(`결과 : ${userInput}`);
+
+      const calculator = new Calculator(userInput);
+
+      Console.print(`결과 : ${calculator.calculate()}`);
     } catch (error) {
       const formattingErrorMsg = new Error(`[ERROR]${error.message}`);
       Console.print(formattingErrorMsg.message);
---
+++ src/Calculator.js
@@ -0,0 +1,10 @@
+//실행 과정을 담기 위한 클래스
+class Calculator {
+  //APP.js에서 입력값을 받아 생성
+  constructor(userInput) {
+    this.userInput = userInput || '';
+  }
+  //실행부
+  calculate() {}
+}
+export default Calculator;
