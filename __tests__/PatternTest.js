dfaa086559c9e300a3cd9ad84f1ebb8d57211369
iftype <iftype@naver.com>
2025-10-19 01:52:47 +0900

    test(Pattern): add unit tests for hasTailPattern  method
    hasTailPattern의 성공테스트와 실패테스트를 추가했다

--- __tests__/PatternTest.js
+++ __tests__/PatternTest.js
@@ -1,13 +1,27 @@
 import Pattern from '../src/Pattern';
-describe('Pattern.js 테스트', () => {
+
+describe('Pattern.js isStartHeadPattern 테스트', () => {
   test(`성공 테스트`, () => {
-    const string = '//*\n';
+    const string = '//*\\n';
     const result = Pattern.isStartHeadPattern(string);
     expect(result).toBe(true);
   });
   test(`실패 테스트`, () => {
-    const string = '/*\n';
+    const string = '/*\\n';
     const result = Pattern.isStartHeadPattern(string);
+    expect(result).toBe(false);
+  });
+});
+
+describe('Pattern.js 테스트', () => {
+  test(`성공 테스트`, () => {
+    const string = '//*\\n';
+    const result = Pattern.hasTailPattern(string);
+    expect(result).toBe(true);
+  });
+  test(`실패 테스트`, () => {
+    const string = '//*\\';
+    const result = Pattern.hasTailPattern(string);
     expect(result).toBe(false);
   });
 });
