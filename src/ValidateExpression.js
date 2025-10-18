5136d83e249521e97c13508b0836070ae04e0d20
iftype <iftype@naver.com>
2025-10-19 06:09:34 +0900

    feat(ValidateExpression): add vaildIsEndNumber method
    표현식의 끝 부분이 숫자인지 판별

--- src/ValidateExpression.js
+++ src/ValidateExpression.js
@@ -6,10 +6,22 @@ class ValidateExpression {
   }
 
   //표현식의 첫 부분이 숫자인지 판별 리팩토링때 utils로 뺄 생각 중
+  //,1,2,3 Error
   vaildIsStartNumber() {
     if (!Number.isInteger(Number(this.string[0])))
       throw new Error('식의 첫 부분은 숫자여야 합니다');
     return this;
+  }
+  //표현식의 끝 부분이 숫자인지 판별 리팩토링때 utils로 뺄 생각 중
+  //1,2,3, Error
+  vaildIsEndNumber() {
+    if (!Number.isInteger(Number(this.string[this.string.length - 1])))
+      throw new Error('식은 숫자로 끝나야 합니다');
+    return this;
+  }
+  validate() {
+    //표현식 파트가 없으면 검증할 필요없음, ""는 0임
+    if (this.string.length === 0) return;
   }
 }
 
