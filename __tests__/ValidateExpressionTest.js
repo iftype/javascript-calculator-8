d6a0b6d45e2ebf62089883bfba960c2e98a5d60a
iftype <iftype@naver.com>
2025-10-19 06:06:57 +0900

    test(ValidateExpression): add unit tests for vaildIsStartNumber method
    첫부분이 숫자로 시작하는 검증 메소드 유닛테스트

---
+++ __tests__/ValidateExpressionTest.js
@@ -0,0 +1,15 @@
+import ValidateExpression from '../src/ValidateExpression.js';
+
+const valid = new ValidateExpression();
+
+const vaildIsStartNumber = valid.vaildIsStartNumber.name;
+describe(vaildIsStartNumber, () => {
+  test('에러발생 테스트', () => {
+    const validator = new ValidateExpression(',1,2,3');
+    expect(() => validator.vaildIsStartNumber()).toThrow(Error);
+  });
+  test('패스 테스트', () => {
+    const validator = new ValidateExpression('1,2,3,,');
+    expect(() => validator.vaildIsStartNumber()).not.toThrow();
+  });
+});
