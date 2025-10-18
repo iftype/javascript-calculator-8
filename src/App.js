6665de06bb30862a094cfa3227d982da6e228c53
iftype <iftype@naver.com>
2025-10-19 01:27:13 +0900

    feat: 인터페이스 기능 추가
    콘솔 입출력, 에러확인 가능
    
    과제 라이브러리 mission-utils 를 사용하여 입출력과 에러메세지를 추가하였다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const userInput = await Console.readLineAsync(
+        '덧셈할 문자열을 입력해 주세요.\n',
+      );
+      Console.print(`결과 : ${userInput}`);
+    } catch (error) {
+      const formattingErrorMsg = new Error(`[ERROR]${error.message}`);
+      Console.print(formattingErrorMsg.message);
+      throw formattingErrorMsg;
+    }
+  }
 }
 
 export default App;
