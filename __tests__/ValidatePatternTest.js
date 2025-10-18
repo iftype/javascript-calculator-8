0961d466de282689f68acf5d8c7a27bd3930131d
iftype <iftype@naver.com>
2025-10-19 04:03:21 +0900

    test(ValidatePattern): add unit tests for validTailPattern method
    validTailPattern의  unit test를 만들었다
    
    + 오탈자 수정 string -> customPart

--- __tests__/ValidatePatternTest.js
+++ __tests__/ValidatePatternTest.js
@@ -10,10 +10,22 @@ describe(validHeadPattern, () => {
   });
   test('성공 테스트', () => {
     const validator = new ValidatePattern('//?');
-    expect(() => validator.validate()).not.toThrow();
+    expect(() => validator.validHeadPattern()).not.toThrow();
   });
-  test('빈문자일때', () => {
-    const validator = new ValidatePattern('');
-    expect(() => validator.validate()).not.toThrow();
+});
+
+const validTailPattern = valid.validTailPattern.name;
+describe(validTailPattern, () => {
+  test('실패 테스트', () => {
+    const validator = new ValidatePattern('//d\t1,2,3');
+    expect(() => validator.validTailPattern()).toThrow(Error);
+  });
+});
+
+const validate = valid.validate.name;
+describe(validate, () => {
+  test('종합 테스트', () => {
+    const validator = new ValidatePattern('//d\t');
+    expect(() => validator.validate()).toThrow(Error);
   });
 });
