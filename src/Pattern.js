dcdcd4d4efc986aa1b637d52cf1ce7cd56a271f9
iftype <iftype@naver.com>
2025-10-19 01:44:34 +0900

    feat(Pattern): add isStartHeadPattern method
    패턴 클래스의 static method 추가
    
    문자열의 앞부분이 기준자와 같으면 true 아니면 false를 반환한다

--- src/Pattern.js
+++ src/Pattern.js
@@ -1,3 +1,8 @@
 import { HEAD_PATTERN, TAIL_PATTERN } from './constants/patternConstants.js';
-class Pattern {}
+class Pattern {
+  //해당 문자열이 HEAD_PATTERN으로 시작하는지 확인
+  static isStartHeadPattern(string) {
+    return string.startsWith(HEAD_PATTERN);
+  }
+}
 export default Pattern;
