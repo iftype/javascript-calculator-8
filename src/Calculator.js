f50b72c6e2c6c7bf0e69946d6d6408c9c434df49
iftype <iftype@naver.com>
2025-10-19 03:23:35 +0900

    feat(Calculator): 커스텀 파트 추출 및 검증 연결
    실제 객체들을 조립하여 테스트가 가능하도록 연결해주었다

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,3 +1,6 @@
+import Extractor from './Extractor.js';
+import ValidatePattern from './ValidatePattern.js';
+
 //실행 과정을 담기 위한 클래스
 class Calculator {
   //APP.js에서 입력값을 받아 생성
@@ -5,6 +8,13 @@ class Calculator {
     this.userInput = userInput || '';
   }
   //실행부
-  calculate() {}
+  calculate() {
+    //추출 객체 생성해서, 커스텀 파트 부분 추출
+    const customDelimPart = Extractor.extractCustomPart(this.userInput);
+    console.log('커스텀파트는', customDelimPart);
+    //커스텀 파트 유효성 검사
+    const validPattern = new ValidatePattern(customDelimPart);
+    validPattern.validate();
+  }
 }
 export default Calculator;
