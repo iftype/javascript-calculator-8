a88ed3c8677171265224306a2cabd2cad555862d
iftype <iftype@naver.com>
2025-10-19 08:19:49 +0900

    feat(Calculator): 클래스 조합 및 계산 함수 추가
    각 기능들을 같은 곳에서 조합

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,20 +1,47 @@
 import Extractor from './Extractor.js';
+import Delimiter from './Delimiter.js';
 import ValidatePattern from './ValidatePattern.js';
-
+import ValidateExpression from './ValidateExpression.js';
 //실행 과정을 담기 위한 클래스
 class Calculator {
   //APP.js에서 입력값을 받아 생성
   constructor(userInput) {
+    this.delimiter = new Delimiter();
     this.userInput = userInput || '';
   }
   //실행부
   calculate() {
-    //추출 객체 생성해서, 커스텀 파트 부분 추출
+    // PART 커스텀 구분자
+    // 유효성 검사 |  커스텀 구분자
+    // 파트 나누기
+    //커스텀 문자 추출하고 더하기
     const customDelimPart = Extractor.extractCustomPart(this.userInput);
-    console.log('커스텀파트는', customDelimPart);
-    //커스텀 파트 유효성 검사
+    const customDelim = Extractor.extractCustomDelimiter(customDelimPart);
+    this.delimiter.addDelimiter(customDelim);
+
+    const expressionPart = Extractor.extractExpressionPart(this.userInput);
+
+    // 2-2 유효성 검사 |  표현식
     const validPattern = new ValidatePattern(customDelimPart);
     validPattern.validate();
+    const validateExpression = new ValidateExpression(expressionPart);
+    validateExpression.validate();
+
+    // // 4.계산 계산위해서 배열 공백
+    const tokenList = this.delimiter.splitDelimExpression(expressionPart);
+    const result = this.sumNumberList(tokenList);
+    // // 5. 결과 출력
+    return result;
+  }
+  // 표현식 문자열을 구분자로 분리함
+  // 공백이 포함된 토큰배열을 받아 조립하여 더함
+  sumNumberList(tokenList) {
+    console.log('계산', tokenList);
+    return tokenList
+      .join('')
+      .split(' ')
+      .map(Number)
+      .reduce((acc, cur) => acc + cur);
   }
 }
 export default Calculator;
