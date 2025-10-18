2c45e6adce4553cdb1aa246fbb94d2de1114f687
iftype <iftype@naver.com>
2025-10-19 04:44:24 +0900

    feat(Delimiter): add hasDelimiterString method
    구분자의 존재여부를 파악하는 메서드

--- src/Delimiter.js
+++ src/Delimiter.js
@@ -3,6 +3,10 @@ class Delimiter {
   constructor() {
     this.delimList = [',', ':'];
   }
+  //해당 문자가 구분자인지 확인
+  hasDelimiterString(delim) {
+    return this.delimList.includes(delim);
+  }
 }
 
 export default Delimiter;
