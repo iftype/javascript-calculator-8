29c1e34d085612c0d72f220edbc055b10474e5a2
iftype <iftype@naver.com>
2025-10-19 02:15:28 +0900

    feat(Extractor): add isNumber method
    안전한 정수 판별 메소드 추가
    
    해당 element의 타입을 Number타입으로 바꾸고 안전한 정수인지 확인, 공백이라면 false를 출력한다

--- src/Extractor.js
+++ src/Extractor.js
@@ -1,5 +1,11 @@
 import Pattern from './Pattern.js';
 import { HEAD_PATTERN, TAIL_PATTERN } from './constants/patternConstants.js';
-class Extractor {}
+class Extractor {
+  //해당 element의 타입을 Number타입으로 바꾸고 안전한 정수인지 확인, 공백이라면 false를 출력한다
+  static isNumber(element) {
+    if (element.trim() === '') return false;
+    return Number.isSafeInteger(Number(element));
+  }
+}
 
 export default Extractor;
