c23c1f8fd10262368d158286c5a12c8e98b9e673
iftype <iftype@naver.com>
2025-10-19 09:55:47 +0900

    refactor(Caculator): 순서 정리 및 변수 제거
    불필요한 변수제거하였습니다

--- src/Calculator.js
+++ src/Calculator.js
@@ -11,37 +11,26 @@ class Calculator {
   }
   //실행부
   calculate() {
-    // PART 커스텀 구분자
-    // 유효성 검사 |  커스텀 구분자
-    // 파트 나누기
-    //커스텀 문자 추출하고 더하기
     const customDelimPart = Extractor.extractCustomPart(this.userInput);
     const customDelim = Extractor.extractCustomDelimiter(customDelimPart);
-    this.delimiter.addDelimiter(customDelim);
-
     const expressionPart = Extractor.extractExpressionPart(this.userInput);
 
-    // 2-2 유효성 검사 |  표현식
-    const validPattern = new ValidatePattern(customDelimPart);
-    validPattern.validate();
-    const validateExpression = new ValidateExpression(expressionPart);
-    validateExpression.validate();
+    new ValidatePattern(customDelimPart).validate();
+    this.delimiter.addDelimiter(customDelim);
+    new ValidateExpression(expressionPart).validate();
 
-    // // 4.계산 계산위해서 배열 공백
     const tokenList = this.delimiter.splitDelimExpression(expressionPart);
-    const result = this.sumNumberList(tokenList);
-    // // 5. 결과 출력
-    return result;
+    return this.sumNumberList(tokenList);
   }
+
   // 표현식 문자열을 구분자로 분리함
   // 공백이 포함된 토큰배열을 받아 조립하여 더함
   sumNumberList(tokenList) {
-    console.log('계산', tokenList);
     return tokenList
       .join('')
       .split(' ')
       .map(Number)
-      .reduce((acc, cur) => acc + cur);
+      .reduce((acc, cur) => acc + cur, 0);
   }
 }
 export default Calculator;
