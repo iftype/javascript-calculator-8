ff2a0f96ea550b44e42e13a9cd61cf243b8e28e3
iftype <iftype@naver.com>
2025-10-19 13:30:20 +0900

    refactor(Calculator): this를 사용하지 않는 메서드 static처리
    9.7 class-methods-use-this

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,21 +1,23 @@
 import Extractor from './Extractor.js';
 import Delimiter from './Delimiter.js';
-import ValidatePattern from './Validate/ValidatePattern.js';
-import ValidateExpression from './Validate/ValidateExpression.js';
-//실행 과정을 담기 위한 클래스
+import Validate from './Validate/Validate.js';
+// 실행 과정을 담기 위한 클래스
 class Calculator {
   constructor(userInput) {
     this.delimiter = new Delimiter();
     this.userInput = userInput || '';
   }
+
   calculate() {
+    // 1. 커스텀 문자파트 , 커스텀 문자, 표현식 파트 추출
     const customDelimPart = Extractor.extractCustomPart(this.userInput);
     const customDelim = Extractor.extractCustomDelimiter(customDelimPart);
     const expressionPart = Extractor.extractExpressionPart(this.userInput);
 
-    new ValidatePattern(customDelimPart).validate();
     this.delimiter.addDelimiter(customDelim);
-    new ValidateExpression(expressionPart).validate();
+
+    // 3. 커스텀 문자파트, 표현식 파트 검사 시작
+    new Validate(expressionPart, customDelimPart, this.delimiter).validate();
 
     const tokenList = this.delimiter.splitDelimExpression(expressionPart);
     return this.sumNumberList(tokenList);
@@ -23,7 +25,8 @@ class Calculator {
 
   // 표현식 문자열을 구분자로 분리함
   // 공백이 포함된 토큰배열을 받아 조립하여 더함
-  sumNumberList(tokenList) {
+  static sumNumberList(tokenList) {
+    console.log(tokenList.join(''));
     return tokenList
       .join('')
       .split(' ')
