ec0dad6e7b36bb2dca711a83a9f83d70215d16aa
iftype <iftype@naver.com>
2025-10-19 09:52:42 +0900

    fix(utills): utils 추가에 따른 버그 수정
    utils를 추가하며 생긴 부수효과를 줄였다

--- __tests__/ExtractorTest.js
+++ __tests__/ExtractorTest.js
@@ -1,24 +1,5 @@
 import Extractor from '../src/Extractor';
 
-const isNumber = Extractor.isNumber.name;
-describe(isNumber, () => {
-  test(`성공 테스트`, () => {
-    const element = '-1';
-    const result = Extractor.isNumber(element);
-    expect(result).toBe(true);
-  });
-  test(`실패 테스트`, () => {
-    const element = ' ';
-    const result = Extractor.isNumber(element);
-    expect(result).toBe(false);
-  });
-  test(`실패 테스트`, () => {
-    const element = '*';
-    const result = Extractor.isNumber(element);
-    expect(result).toBe(false);
-  });
-});
-
 const indexOfNumber = Extractor.indexOfNumber.name;
 describe(indexOfNumber, () => {
   test(`성공 테스트`, () => {
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,5 @@
 //안전한 정수인지 검사
 export function isNumber(params) {
+  if (params === null || params === undefined || params === '') return false;
   return Number.isSafeInteger(Number(params));
 }
