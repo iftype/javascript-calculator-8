33769a4b8ccd648913d93edabf2c461c2397391d
iftype <iftype@naver.com>
2025-10-19 13:30:43 +0900

    docs: 주석처리

--- src/Delimiter.js
+++ src/Delimiter.js
@@ -1,20 +1,22 @@
-//구분자들을 관리하는 클래스
-//싱글톤 적용, 커스텀 구분자가 초기화되는 현상 때문
+// 구분자들을 관리하는 클래스
+// 싱글톤 적용, 커스텀 구분자가 초기화되는 현상 때문
 class Delimiter {
   constructor() {
     this.delimList = [',', ':'];
   }
-  //해당 문자가 구분자인지 확인
+
+  // 해당 문자가 구분자인지 확인
   hasDelimiterString(delim) {
     return this.delimList.includes(delim);
   }
+
   // 구분자 추가
   addDelimiter(delim) {
     this.delimList.push(delim);
   }
 
-  //표현식에 있는 기호들 중 구분자를 찾아 공백으로 만들어줌
-  //공백으로 만든 다음 유효성 검사 할 생각
+  // 표현식에 있는 기호들 중 구분자를 찾아 공백으로 만들어줌
+  // 공백으로 만든 다음 유효성 검사 할 생각
   splitDelimExpression(string) {
     return string
       .split('')
--- src/Extractor.js
+++ src/Extractor.js
@@ -1,22 +1,22 @@
 import Pattern from './Pattern.js';
 import { HEAD_PATTERN, TAIL_PATTERN } from './constants/patternConstants.js';
-import { isNumber } from './utils/Utils.js';
+import { isNumber } from './utils/utils.js';
 
 class Extractor {
-  //처음 숫자를 만나는 인덱스를 리턴
+  // 처음 숫자를 만나는 인덱스를 리턴
   static indexOfNumber(string) {
     return string.split('').findIndex((e) => isNumber(e));
   }
 
-  //숫자를 만나는 곳을 찾아 첫 부분부터 잘라낸다, 만약 숫자를 찾아내지 못한다면 원래 문자열을 리턴한다
+  // 숫자를 만나는 곳을 찾아 첫 부분부터 잘라낸다, 만약 숫자를 찾아내지 못한다면 원래 문자열을 리턴한다
   static extractCustomPart(string) {
     const findNum = this.indexOfNumber(string);
     return findNum === -1 ? string : string.slice(0, findNum);
   }
 
-  //커스텀파트를 탐색하여 HEAD_PATTERN이 끝나는 지점부터 TAIL_PATTERN 까지의 문자열을 가져온다
-  //하지만 문자열의 시작부터 숫자를 만나는 지점까지 기준자가 둘다 없다면 원래 문자열을 가져온다
-  //커스텀 문자가 없을 수 있기 때문
+  // 커스텀파트를 탐색하여 HEAD_PATTERN이 끝나는 지점부터 TAIL_PATTERN 까지의 문자열을 가져온다
+  // 하지만 문자열의 시작부터 숫자를 만나는 지점까지 기준자가 둘다 없다면 원래 문자열을 가져온다
+  // 커스텀 문자가 없을 수 있기 때문
   static extractCustomDelimiter(customPart) {
     if (!Pattern.hasPatterns(customPart)) return customPart;
     const headIndex = customPart.indexOf(HEAD_PATTERN) + HEAD_PATTERN.length;
@@ -24,8 +24,8 @@ class Extractor {
     return customPart.slice(headIndex, tailIndex);
   }
 
-  //숫자를 만나는 곳을 찾아 숫자부분 부터 끝까지 잘라낸다.
-  //숫자를 찾지 못했다면 받은 문자를 리턴한다
+  // 숫자를 만나는 곳을 찾아 숫자부분 부터 끝까지 잘라낸다.
+  // 숫자를 찾지 못했다면 받은 문자를 리턴한다
   static extractExpressionPart(string) {
     const findNum = this.indexOfNumber(string);
     if (findNum === -1) return string;
