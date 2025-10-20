6cd6f0402298caf1f59e150a7a08ea7be5b77b1b
iftype <iftype@naver.com>
2025-10-20 20:29:10 +0900

    fix(sumTokenList): 에러 로그 추가

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -14,9 +14,11 @@ export function isNumber(params) {
 // 공백이 포함된 토큰배열을 받아 조립하여 더함
 // ["1"," ","2","2"," ","3"] -> 1 22 3 -> [1,,22,,3]
 export function sumTokenList(tokenList) {
-  return tokenList
+  const result = tokenList
     .join('')
     .split(SPACE)
     .map(Number)
     .reduce((acc, cur) => acc + cur, 0);
+  if (isNaN(result)) throw new Error('의도치않은에러');
+  return result;
 }
